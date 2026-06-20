03e7d1abe449e479f26c25b3626cc1b13e7f2d0f
Lokkanet <113406930+bagheeraja@users.noreply.github.com>
2026-06-20 15:28:01 -0400

    BaristaForm ingredient categories and ingredient options created

--- src/components/BaristaForm.jsx
+++ src/components/BaristaForm.jsx
@@ -1,9 +1,46 @@
 import { useState } from "react";
 const BaristaForm = () => {
 
+    const [inputs, setInputs] = useState({
+        'temperature': '',
+        'milk': '',
+        'syrup': '',
+        'blended': '',
+    });
+
+    const ingredients = {
+        'temperature' : ['hot', 'lukewarm', 'cold'],
+        'syrup': ['mocha', 'vanilla', 'toffee', 'maple', 'caramel', 'other', 'none'],
+        'milk': ['cow', 'oat', 'goat', 'almond', 'none'],
+        'blended': ['yes', 'turbo', 'no']
+    }
+
+    const onNewDrink = () => {
+
+    }
+
+    const onCheckAnswer = () => {
+
+    }
+
     return (
         <div>
+            <h2>Hi, I'd like to order:</h2>
 
+            <form>
+
+            </form>
+            <button 
+                className="button submit"  
+                onClick={onCheckAnswer}>
+                    Check Answer
+            </button>
+
+            <button 
+                className="button submit" 
+                onClick={onNewDrink}>
+                    New Drink
+            </button>
         </div>
     )
 
