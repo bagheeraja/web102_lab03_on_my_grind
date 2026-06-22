2457e63225db75cdc6d6f44b42357f110a9b2053
Lokkanet <113406930+bagheeraja@users.noreply.github.com>
2026-06-22 12:43:59 -0400

    RecipeChoices component updated to allow text input for guesses instead of radio buttons

--- src/components/RecipeChoices.jsx
+++ src/components/RecipeChoices.jsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
 
-const RecipeChoices = ({handleChange, label, choices, checked}) => {
+const RecipeChoices = ({handleChange, label, choices, checked, currentVal}) => {
     return(
         <div className="radio-buttons">
+            <input
+                type="text"
+                name={label}
+                value={currentVal}
+                placeholder="Guess the ingredient..."
+                onChange={handleChange}
+                className="textbox"
+            />
+            
             {choices &&
                 choices.map((choice) => (
                     <li key={choice}>
-                        <input
-                            id={choice}
-                            value={choice}
-                            name={label}
-                            type="radio"
-                            onChange={handleChange}
-                            checked={checked == choice}
-                    />
+
                         {choice}
+
                     </li>
                 ))}
         </div>
